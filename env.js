e7d3d859ba5da7653ff6e20deeff59e82ab53937
vrushang desai <moonshot@moonshot.local>
2019-07-30 06:08:50 +0530

    added config

---
+++ env.js
@@ -0,0 +1,15 @@
+// put real values generated in JSON file instead of X's
+// also rename the file to "env.js" when in use
+
+export const dialogflowConfig = {
+	"type": "service_account",
+  "project_id": "newagent-wspffe",
+  "private_key_id": "5f5efd1c402a49a999d5dc8f2b8578e591fee4df",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC4NpGmmvQ1ZP7U\nAS0mbU7tzksHNozM/hYsYaeObsQhwbXhGzCqwyZehGnbrm3hMTfjm5/XaHUoqHvn\nFG2KN7DNb0Du9w8Kkzg69QHoiJ2e3ou38Adz3slDu3qaG27G/CTfLn9BKK6Hpnyu\nU3T7SRaZm0v/GKpEm4pbYmk5xPAJsatyLNwMPQHtyZvTgYkmjAyIHqrKJaZiT2O+\nnO/mrJBXIglEj4x+MLpyznz0fr7mtrlQMcpiqlQePjqwKDaxDEfvNbfrEOHppv0c\nQxeqLCqcrnMfcBHj+vcIijoDbIjX8NV/v39hufpRwZCXbepA/utVAJ/YboOE0XBK\n9hOBPkcPAgMBAAECggEABcZcKYtpBXV9kZrZ1f8OPuE9M+QvZMvAWTLUhGAPkPlY\nIM7v5yKIBOWExgjBVBJtbJq315JkPdqOS8+1oWQejEolEHi5RX0ZAawlm6sQuebD\nHYiOS0EcMR3i+YrLnbxQ0ZhND/ddBU6gUPy3yZrgdP9L0hrtV4pCu5uprdBxL/SO\nn+8HQyGLg0iGdAcdXTqS4T195LGNP2eEESJF61JQhB0GhfIhMK4velrnzn773hJH\nrUxTAlofvqqJotzqp4rX/l9KtisV7/CFQRDh9DXx1MlHglCcyXSB4OPXM1o1gHRO\n9HKUJe906A6WPlOfHyOYCJ1fztBzp4NEsMte23sjaQKBgQD+q5nyX39Xxn2PJHcg\njoeJQMxP5RAXgQgxYlM8fIgX+Q4uxSyTzuF9UORBlWwXkvmN2IqfOg4lpd6sr0nw\ny7pZMQiGwolY7ViL62arAYns3gE5xQrzxFXGtRhJNTXlM/xqmYF6zpFSeJ1ylOMd\niW4Cg477gbv2m1LnR7PgwQzp2QKBgQC5LMr13bwG4XOU/peZVWybOdFvXoSmf342\nlBF/YWQZ3StDG09kPf9JBvPeXcpmZrkG4uADI3wVc2qLZipHlUMJsV52cYK5hBF8\nhOPGDQPtCN1owN34q+nN5rNI0IemfGqdxLguQNguS3dwsiB0xq2730lXjwx03io3\nA9+94+R/JwKBgQCAadroggreIbIyOXrwKa5iIRBkwOYKU//jlubNuyWMoQLrxnSh\n21GMNPvDriqhAHVhduz/OPDlPuw3OvQXvUKrsrPP78v90UAy3XIZjF8V1yPbE2Zg\nYIG3/pzOXUxd1IbU4a6OVBam9Ta/DiAvRpr4HYo9e79hrBRzlo43KGDwkQKBgBFU\nLMYncvId5QgbbyptQRWhqoTGjplaMwB+iMin34tAFE1aX+qclqhJ20aC9m7wy/3l\n+1qv1mvXejADZo9+W2Cr1ZA1/SrJPi2t7fbZvvVOsBGegudDh0S6gyqcQSWGXR/Y\niffiH5jtfQbx1HG5Kgb71Uw5WcwSAldVqHmHuxlfAoGBAKCniw4SOEYIZ6wrMEDO\nUclLy5O3qw6aTMx3aMH6UFCcUbwwnQDF4PgbOsEKBzQq0dxmtDuPd2fknfq6Y3vc\nNYNcl0SWpgT6UFXMy2k1ECkT2Gq9t+Wmevuu3/ye83x0xCAN9jr2jiHTNcTzR4O7\nzlOmwtyavcRifgs4K0ck6J5r\n-----END PRIVATE KEY-----\n",
+  "client_email": "newagent-wspffe@appspot.gserviceaccount.com",
+  "client_id": "117395108812206568986",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/newagent-wspffe%40appspot.gserviceaccount.com"
+};
